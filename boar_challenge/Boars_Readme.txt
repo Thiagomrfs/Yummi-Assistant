35f8ccd55301cdb9b9883704c8e8b55332760c44
Thiagomrfs <thiagomarbsn@gmail.com>
2020-07-07 20:00:46 -0300

    Boar challenge

---
+++ boar_challenge/Boars_Readme.txt
@@ -0,0 +1,23 @@
+Dependências:
+	pip install wikipedia
+	pip install pyttsx3==2.6
+
+Usabilidade:
+	Assistente pessoal, vai realizar algumas tarefas pra você:
+		open website - abre um websitte específico, quando for registrar um novo utilize a URI:
+			http://seila.com
+
+		translate - tradutor com suporte para 101 linguas, index português = 75, index inglês = 32 eu acho.
+
+		get passwords - consulta todas as senhas armazenadas no guarda-senha, necessita do main password
+				para que as senhas sejam mostradas corretamente.
+
+		add password - adiciona uma senha. ATENÇÃO: VOCÊ DEVE USAR O MESMO MAIN PASSWORD QUANDO FOR CONSULTAR
+							    AS SENHAS. PORTANTO USE ALGO SIMPLES, PODE ATÉ SER "senha".
+
+		add birthday reminder - adiciona lembrete de aniversários próximos. ATENÇÃO: AO COLOCAR O DIA OU O MÊS UTILIZE
+											     APENAS OS CARACTERES NECESSÁRIOS.
+											     FAZER: dia - 9
+											     NÃO FAZER: dia - 09
+
+	Só isso que deu tempo fazer, fiquei um dia sem pc, quase morro, press F to pay respect.
--- boar_challenge/main.py
+++ boar_challenge/main.py
@@ -53,100 +53,104 @@ def show_near_birthdays():
 
 
 wish_me()
-show_near_birthdays()
-resp, option = support.menu(["Open website",
-                            "Search meaning",
-                            "Translate",
-                            "Get passwords",
-                            "Add password",
-                            "Add birthday reminder",
-                            "Exit"])
+while True:
+    show_near_birthdays()
+    resp, option = support.menu(["Open website",
+                                "Search meaning",
+                                "Translate",
+                                "Get passwords",
+                                "Add password",
+                                "Add birthday reminder",
+                                "Exit"])
 
-os.system("cls")
-speak(f"You choosed: {option}")
+    os.system("cls")
+    speak(f"You choosed: {option}")
 
 
-if option == "Open website":
-    support.msg("open website")
-    websites = memory.get_websites_in_memory()
-    query = []
+    if option == "Open website":
+        support.msg("open website")
+        websites = memory.get_websites_in_memory()
+        query = []
 
-    for website in websites:
-        for key in website:
-            query.append(key)
-    query.append("add new website")
-    query.append("remove website")
-    selected, option = support.menu(query)
+        for website in websites:
+            for key in website:
+                query.append(key)
+        query.append("add new website")
+        query.append("remove website")
+        selected, option = support.menu(query)
 
-    for website in websites:
-        for site, url in website.items():
-            if site == option:
-                speak("opening website")
-                webbrowser.open(url, new=2)
-    if option == "add new website":
-        details = {}
-        name = input("What's the name of the website? ")
-        print("\033[32mTip: http:/mywebsite.com\033[m")
-        url = input("what's the url of the website? ")
-        details[name] = url
-        memory.add_website_to_memory(details)
-    elif option == "remove website":
-        support.support.clear_screen()
-        support.msg("which website do you want to remove?")
-        index, removed_one = support.menu(query[:-2])
         for website in websites:
             for site, url in website.items():
-                if site == removed_one:
-                    memory.remove_website_from_memory(website)
-    support.clear_screen()
-elif option == "Search meaning":
-    support.msg("Search meaning")
-    term = input("Wich term do you want to search about? ")
-    speak("searching")
-    response = wikipedia.summary(term, sentences=2)
-    print()
-    print(response)
-    speak(response)
-    support.clear_screen()
-elif option == "Translate":
-    output = support.translate()
-    print(f"The translated text is: {output}")
-elif option == "Get passwords":
-    support.msg("Password manager")
-    print("\033[31mBe sure to write it correctly!!!\033[m")
-    main_password = input("Insert your main password: ")
-    data = memory.get_passwords_from_memory()
-    keys = []
-    for entry in data:
-        for key, value in entry.items():
-            decripted = cipher(value, main_password, 2)
-            print(f"\033[34m{key}\033[m - \033[33m{decripted}\033[m")
-    print("\033[mIf the passwords here has no sense, probably your main password is wrong!\033[m")
-elif option == "Add password":
-    support.msg("Adding new password")
-    query = {}
-    print("\033[31mBe sure to remember this, you can't change it later!!!\033[m")
-    print("\033[31mAnd you need it to get the passwords, so write it out!!!\033[m")
-    main_password = input("Insert your main password: ")
-    query["site"] = input("For wich site do you want to store te password? ")
-    query["password"] = input("Insert the password: ")
-    memory.add_password_to_memory(query, main_password)
-elif option == "Add birthday reminder":
-    support.msg("Adding birthday reminder")
-    query = {}
-    today = datetime.date.today()
-    who = input("it's whose birthday? [name] ")
-    day = support.verify_int(input("what day will it happen? "), range(1, 31))
-    month = support.verify_int(input("what month will it happen? "), range(1, 13))
-    year = 0
-    if today.day > day:
-        if today.month > month:
-            year = today.year + 1
+                if site == option:
+                    speak("opening website")
+                    webbrowser.open(url, new=2)
+        if option == "add new website":
+            details = {}
+            name = input("What's the name of the website? ")
+            print("\033[32mTip: http:/mywebsite.com\033[m")
+            url = input("what's the url of the website? ")
+            details[name] = url
+            memory.add_website_to_memory(details)
+        elif option == "remove website":
+            support.support.clear_screen()
+            support.msg("which website do you want to remove?")
+            index, removed_one = support.menu(query[:-2])
+            for website in websites:
+                for site, url in website.items():
+                    if site == removed_one:
+                        memory.remove_website_from_memory(website)
+    elif option == "Search meaning":
+        support.msg("Search meaning")
+        term = input("Wich term do you want to search about? ")
+        speak("searching")
+        response = wikipedia.summary(term, sentences=2)
+        print()
+        print(response)
+        speak(response)
+        support.clear_screen()
+    elif option == "Translate":
+        output = support.translate()
+        print(f"The translated text is: {output}")
+        sleep(10)
+        support.clear_screen()
+    elif option == "Get passwords":
+        support.msg("Password manager")
+        print("\033[31mBe sure to write it correctly!!!\033[m")
+        main_password = input("Insert your main password: ")
+        data = memory.get_passwords_from_memory()
+        keys = []
+        for entry in data:
+            for key, value in entry.items():
+                decripted = cipher(value, main_password, 2)
+                print(f"\033[34m{key}\033[m - \033[33m{decripted}\033[m")
+        print("\033[mIf the passwords here has no sense, probably your main password is wrong!\033[m")
+    elif option == "Add password":
+        support.msg("Adding new password")
+        query = {}
+        print("\033[31mBe sure to remember this, you can't change it later!!!\033[m")
+        print("\033[31mAnd you need it to get the passwords, so write it out!!!\033[m")
+        main_password = input("Insert your main password: ")
+        query["site"] = input("For wich site do you want to store te password? ")
+        query["password"] = input("Insert the password: ")
+        memory.add_password_to_memory(query, main_password)
+    elif option == "Add birthday reminder":
+        support.msg("Adding birthday reminder")
+        query = {}
+        today = datetime.date.today()
+        who = input("it's whose birthday? [name] ")
+        day = support.verify_int(input("what day will it happen? "), range(1, 31))
+        month = support.verify_int(input("what month will it happen? "), range(1, 13))
+        year = 0
+        if today.day > day:
+            if today.month > month:
+                year = today.year + 1
+            else:
+                year = today.year
         else:
             year = today.year
-    else:
-        year = today.year
-    query[who] = [year, month, day]
-    memory.add_birthday_reminder(query)
-elif option == "Exit":
-    exit()
+        query[who] = [year, month, day]
+        memory.add_birthday_reminder(query)
+    elif option == "Exit":
+        exit()
+    
+    support.clear_screen()
